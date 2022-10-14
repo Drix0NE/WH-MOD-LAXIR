c1b9b41c3f06f7c4a916c8b2940443d985264382
LaxirMods <115714063+LaxirMods@users.noreply.github.com>
2022-10-14 10:24:12 +0800

    Update creator2.js

--- plugins/creator2.js
+++ plugins/creator2.js
@@ -15,7 +15,7 @@ if (command == 'whmods2') {
   let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;${author};;;\nFN:${author}\nORG:${author}\nTITLE:\nitem1.TEL;waid=6285332039348:+62 825-3320-3948\nitem1.X-ABLabel:${author}\nX-WA-BIZ-DESCRIPTION:${htjava} Nih pengembang ku kack yg mengaktifkan aq.\nX-WA-BIZ-NAME:${author}\nEND:VCARD`
 await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: fakes })
 }
-if (command =='whmods3')
+if (command =='whmods3') {
 let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;Saya Owner LaxirMODS;Bot;;Md\nFN:Saya Owner Laxir Mods\nNICKNAME:👑 Owner WH Laxir Bot\nORG:WH\nTITLE:MODS\nitem1.TEL;waid=6285332039348:+62 825-3320-3948\nitem1.X-ABLabel:📞 Nomor Owner\nitem2.URL:https://youtube.com/channel/UC4KkbFZ15ILdDhLJeytnFDw\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET:laxirmods@gmail.com\nitem3.X-ABLabel:💌 Mail Owner WH MODS BOT\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel:📍 Lokasi Saya\nBDAY;value=date:👤 "Jika Tidak Ada Bahu Untuk Bersandar Masih ada lantai Untuk Bersujud\nEND:VCARD`
 const tag_own = await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: fakes })
 let caption = `👋 Hai *${name} @${who.split("@")[0]}*, itu nomor ownerku, jangan dispam  kak😊`
